83fa5db692034e9eedcca5452d7ab15bd0b807d2
Elias Roussos <51466260+EliasIsaiah@users.noreply.github.com>
2023-06-14 20:38:16 -0400

    basic implementation of detection of "a" and counter increment

--- before/script.js
+++ before/script.js
@@ -1,12 +1,25 @@
 let counter = 0;
 let inputString = "";
 let numberOfVowels = 0;
-let vowels = ["a","e","i","o","u"];
+let vowels = ["a", "e", "i", "o", "u"];
+
+inputString = "cafeteria";
+
+for (letter of inputString) {
+    console.log(`current letter: ${letter}`);
+    if(letter === "a") {
+        console.log("you've got an \"a\"!");
+        counter++;
+        console.log(`counter: ${counter}`);
+    }
+}
+
+getNumberOfVowels();
 
 function getNumberOfVowels() {
-
+    
 }
 
 function cleanString() {
-    
+
 }
